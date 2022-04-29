e6ad082e5c2fa100bec54c375cedd077de93baed
riyagshah <riyagshah4@gmail.com>
2022-04-29 09:06:22 +0530

    fdfz

---
+++ component/navbar.js
@@ -0,0 +1,24 @@
+function navbar(){
+    return     `<h3>
+    <a href="/">Home</a>
+</h3>
+
+<div id="options">
+<h3>
+    <a href="/jweleery.html">Jewelery</a>
+</h3>
+
+<h3>
+    <a href="/electronic.html">electronic</a>
+</h3>
+<h3>
+    <a href="login.html">Login </a>
+</h3>
+<h3>
+    <a href="signup.html">signup </a>
+</h3>
+
+    `;
+    
+}
+export {navbar};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {navbar} from '../component/navbar.js'
+console.log(navbar)
+import '../styles/navbar.css';
+var nd=document.getElementById('navbar')
+nd.innerHTML=navbar();
